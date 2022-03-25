f62b17bffbb60ca5118cc5bac5dab47318c5bd05
vespenegas <91216338+vespenegas@users.noreply.github.com>
2022-03-24 20:47:47 -0400

    bruteforce

---
+++ bruteforce.py
@@ -0,0 +1,33 @@
+from itertools import combinations
+import pandas as pd
+
+# read csv file to pull data
+df = pd.read_csv('Groceries.csv')
+# drop food group category
+df1 = df[['Food', 'Price', 'weight (lbs)']]
+
+
+# makes all possible combinations
+def anycomb(items):
+    'return combinations of any length from the items '
+    return (comb for r in range(1, len(items) + 1)for comb in combinations(items, r))
+
+
+# gets total value of the combination
+def totalvalue(comb):
+    'Totalise a particular combination of items'
+    total_price = total_weight = 0
+    for item, price, weight in comb:
+        total_price += price
+        total_weight += weight
+    return (total_weight, -total_price) if total_price <= 30 else (0, 0)
+
+
+items = df1.values.tolist()
+
+
+# max weight or min price if values equal
+bagged = max(anycomb(items), key=totalvalue)
+print("Bagged the following items\n  " + '\n  '.join(sorted(item for item, _, _ in bagged)))
+weight, price = totalvalue(bagged)
+print("for a total weight of %i and a total price of %i" % (weight, -price))
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
 # hi
+import pandas as pd
+
+# read csv file to pull data
+df = pd.read_csv('Groceries.csv')
+# drop food group category
+df1 = df[['Food', 'Price', 'weight (lbs)']]
+
+items = df1.values.tolist()
+newlist = []
+print(items)
