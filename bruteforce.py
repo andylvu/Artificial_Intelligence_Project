4f62c904ffdc8822ed4902345b7aa390d1bd5062
vespenegas <91216338+vespenegas@users.noreply.github.com>
2022-03-25 16:20:05 -0400

    bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 import pandas as pd
+import time
 
 # read csv file to pull data
 df = pd.read_csv('Groceries.csv')
@@ -20,14 +21,22 @@ def totalvalue(comb):
     for item, price, weight in comb:
         total_price += price
         total_weight += weight
-    return (total_weight, -total_price) if total_price <= 30 else (0, 0)
+    return (total_weight, -total_price) if total_price <= 100 else (0, 0)
 
 
 items = df1.values.tolist()
+# k = size of list desired
+k = 21
+n = len(items)
+for i in range(0, n - k):
+    items.pop()
 
 
+# start time
+start_time = time.time()
 # max weight or min price if values equal
 bagged = max(anycomb(items), key=totalvalue)
 print("Bagged the following items\n  " + '\n  '.join(sorted(item for item, _, _ in bagged)))
 weight, price = totalvalue(bagged)
 print("for a total weight of %i and a total price of %i" % (weight, -price))
+print("--- %s seconds ---" % (time.time() - start_time))
--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,10 @@
-# hi
+# testing
 import pandas as pd
 
-# read csv file to pull data
+
 df = pd.read_csv('Groceries.csv')
 # drop food group category
 df1 = df[['Food', 'Price', 'weight (lbs)']]
-
 items = df1.values.tolist()
-newlist = []
-print(items)
+items2 = df.head(21)
+print(items2)
