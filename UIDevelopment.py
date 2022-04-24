8d823fd97edad9842ec78d79717a1620f452e136
Jswisss <46788908+Jswisss@users.noreply.github.com>
2022-04-24 19:29:40 -0400

    Add files via upload

--- UIDevelopment.py
+++ UIDevelopment.py
@@ -1,28 +1,43 @@
 # This is a sample Python script.
 from tkinter import *
+from tkinter import ttk
 
+from genetic import *
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 # create window
 root = Tk()
+root.geometry("400x400")
 
 # create Starting Frame for budget setting
 BudgetFrame = Frame(root)
-BudgetFrame.grid(row=0, column=0)
+BudgetFrame.pack(fill=BOTH, expand=1)
 
+my_canvas = Canvas(BudgetFrame)
+my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+my_scrollbar = ttk.Scrollbar(BudgetFrame, orient=VERTICAL, command=my_canvas.yview)
+my_scrollbar.pack(side=RIGHT, fill=Y)
+
+my_canvas.configure(yscrollcommand=my_scrollbar.set)
+my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+
+second_frame = Frame(my_canvas)
+
+my_canvas.create_window((0,0), window=second_frame, anchor="nw")
 def submit():  # Creates submit screen for User Convenience Where you call methods
     saveInputs()
     clear_frame()
-    titleName = Label(BudgetFrame, text="Hello and Welcome to our shopping list Application", font=35)
-    StartingTextEmptyWin2 = Label(BudgetFrame, text="  ")
-    processingLabel = Label(BudgetFrame, text="Form has been submitted.Please wait while we process your order",
+    titleName = Label(second_frame, text="Hello and Welcome to our shopping list Application", font=35)
+    StartingTextEmptyWin2 = Label(second_frame, text="  ")
+    processingLabel = Label(second_frame, text="Form has been submitted.Please wait while we process your order",
                             font=25)
     titleName.grid(row=0, column=0)
     StartingTextEmptyWin2.grid(row=1, column=0)
     processingLabel.grid(row=2, column=0)
     # if list is created delete
-    FinalScreen()
+    shoppinglistResult, totalAmount = test(budgetAmount)
+    FinalScreen(shoppinglistResult, totalAmount)
 
 
 def saveInputs():  # used to update variables
@@ -35,57 +50,55 @@ def saveInputs():  # used to update variables
     grainOption = grain.get()
 
 def clear_frame():
-    for widgets in BudgetFrame.winfo_children():
+    for widgets in second_frame.winfo_children():
         widgets.destroy()
 
-def FinalScreen():  # needs to be connected to algorithm
+def FinalScreen(shoppinglistResult, totalAmount):  # needs to be connected to algorithm
     clear_frame()
-    FinalTotal = 'Your Total is: $' + budgetAmount
-    shoppinglist = ['chicken breast', '80% lean ground beef', 'cucumber', 'avocado', 'red grape', '2% milk',
-                    'english muffins',
-                    'yellow peaches', 'cheetos', 'pear', 'whole carrots', 'bella muchroom', 'pumpkin pie']
-    TitleName = Label(BudgetFrame, text="Hello and Welcome to our shopping list Application", font=35)
-    StartingTextEmptyWin3 = Label(BudgetFrame, text="  ")
+    FinalTotal = 'Your Total is: $' + str(totalAmount)
+    shoppinglist = shoppinglistResult
+    TitleName = Label(second_frame, text="Hello and Welcome to our shopping list Application", font=35)
+    StartingTextEmptyWin3 = Label(second_frame, text="  ")
     TitleName.grid(row=0, column=0)
     StartingTextEmptyWin3.grid(row=1, column=0)
     for i in range(len(shoppinglist)):
-        curListItem = Label(BudgetFrame, text=shoppinglist[i], font=20)
+        curListItem = Label(second_frame, text=shoppinglist[i], font=20)
         curListItem.grid(row=i + 2, column=0)
-    StartingTextEmptyWin3_1 = Label(BudgetFrame, text="  ")
+    StartingTextEmptyWin3_1 = Label(second_frame, text="  ")
     StartingTextEmptyWin3_1.grid(row=len(shoppinglist) + 3, column=0)
-    FinalBudgetAmount = Label(BudgetFrame, text=FinalTotal, font=25)
+    FinalBudgetAmount = Label(second_frame, text=FinalTotal, font=25)
     FinalBudgetAmount.grid(row=(len(shoppinglist) + 4), column=0)
 
 # create a label Widget
-StartingText = Label(BudgetFrame, text="Hello and Welcome to our shopping list Application", font=(30))
-StartingTextEmpty = Label(BudgetFrame, text="  ")
-StartingTextEmpty2 = Label(BudgetFrame, text="  ")
-StartingTextEmpty3 = Label(BudgetFrame, text="  ")
+StartingText = Label(second_frame, text="Hello and Welcome to our shopping list Application", font=(30))
+StartingTextEmpty = Label(second_frame, text="  ")
+StartingTextEmpty2 = Label(second_frame, text="  ")
+StartingTextEmpty3 = Label(second_frame, text="  ")
 
-BudgetText = Label(BudgetFrame, text="Please input a budget amount into the input field below without $")
-budgetInput = Entry(BudgetFrame)
+BudgetText = Label(second_frame, text="Please input a budget amount into the input field below without $")
+budgetInput = Entry(second_frame)
 budgetInput.insert(0, "00.00")
 budgetAmount = budgetInput.get()
 
-ExemptionText = Label(BudgetFrame, text="Please check off any food tree type you don't want in your list")
+ExemptionText = Label(second_frame, text="Please check off any food tree type you don't want in your list")
 # 1 is checked and 0 is unchecked
 meat = IntVar()  # used later for algorithm
-meatCheck = Checkbutton(BudgetFrame, text="Do you not want meat in your shopping list", variable=meat)
+meatCheck = Checkbutton(second_frame, text="Do you not want meat in your shopping list", variable=meat)
 
 vegetable = IntVar()  # used later for algorithm
-vegetableCheck = Checkbutton(BudgetFrame, text="Do you not want vegetables in your shopping list", variable=vegetable)
+vegetableCheck = Checkbutton(second_frame, text="Do you not want vegetables in your shopping list", variable=vegetable)
 
 fruit = IntVar()  # used later for algorithm
-fruitCheck = Checkbutton(BudgetFrame, text="Do you not want fruits in your shopping list", variable=fruit)
+fruitCheck = Checkbutton(second_frame, text="Do you not want fruits in your shopping list", variable=fruit)
 
 diary = IntVar()  # used later for algorithm
-diaryCheck = Checkbutton(BudgetFrame, text="Do you not want diary in your shopping list", variable=diary)
+diaryCheck = Checkbutton(second_frame, text="Do you not want diary in your shopping list", variable=diary)
 
 grain = IntVar()  # used later for algorithm
-grainCheck = Checkbutton(BudgetFrame, text="Do you not want grain like bread or rice in your shopping list",
+grainCheck = Checkbutton(second_frame, text="Do you not want grain like bread or rice in your shopping list",
                          variable=grain)
 
-SubmitButton = Button(BudgetFrame, text="Submit your budget", padx=100, command=submit, fg="white", bg="black")
+SubmitButton = Button(second_frame, text="Submit your budget", padx=100, command=submit, fg="white", bg="black")
 
 # Shoving it onto the screen
 StartingText.grid(row=0, column=0)
--- genetic.py
+++ genetic.py
@@ -5,22 +5,22 @@ import pandas as pd
 "fitness" calculates the fitness of the speciments in the population.
 Returns an array of fitness scores, which is the value achieved given the threshold was not exceeded.
 '''
-def fitness(population, values, costs ,threshold):
-    
+
+
+def fitness(population, values, costs, threshold):
     specimen_values = np.zeros(shape=(len(population))).astype(int)
     specimen_costs = np.zeros(shape=(len(population))).astype(int)
-    
+
     # Iterate over all speciments in the population
-    for i in range(len(population)):    
-        
+    for i in range(len(population)):
         # The total value of all items in ith specimen of the population
         specimen_values[i] = np.sum(population[i] * values)
         # The total cost of all items in the ith specimen
         specimen_costs[i] = np.sum(population[i] * costs)
-    
+
     # Filter only value totals that fit within threshold
     specimen_values[specimen_costs > threshold] = 0
-    
+
     return specimen_values
 
 
@@ -29,19 +29,20 @@ def fitness(population, values, costs ,threshold):
 The function finds the best half of the population and returns them as an array parents.
 In the event that there are no fit parents, np.argsort will simply select the indices of the middle half of the population.
     This will also be the case in selecting supplementary parents if less than half are fit.
-    
+
 Note that population count MUST be even, otherwise int() will round down
 '''
+
+
 def parent_selection(fitness_score, population):
-    
     # Half of the population
     number_of_parents = int(len(population) / 2)
-    
+
     # Get array of indices for parents selected as candidates
     candidate_indices = np.argsort(fitness_score)[::-1][:number_of_parents]
-    
-    return population[candidate_indices]   
-   
+
+    return population[candidate_indices]
+
 
 '''
 "cross_breeding" takes half of one specimen's genes and mixes them with another.
@@ -49,49 +50,46 @@ Two offspring are 'born' for each pair of parents, each containing half of each 
 The two offspring's inhereted genes are not the same, as they are taken from opposite halves of the parent's.
 Parents are mated in decending order, if there are 4 parents, there will be 4 offspring, 2 to each pair.
 This returns a new array of offspring
-
 If the number of parents is odd, the last parent will produce a single offspring with the most fit parent.
-
 '''
+
+
 def cross_breeding(parents):
-    
     # make array of offspring, same shape as parents
     offspring = np.zeros(parents.shape).astype(int)
-    
+
     # Crossover point is half of the 'genes' (items in the database), it is rounded down if odd.
     cp = int(len(parents[0]) / 2)
-    
+
     # Case that the number of offspring is even
-    if len(offspring)%2 == 0:
+    if len(offspring) % 2 == 0:
         for i in range(0, len(offspring), 2):
-            
             # First offspring
             offspring[i][:cp] = parents[i][:cp]
-            offspring[i][cp:] = parents[i+1][cp:]
-            
+            offspring[i][cp:] = parents[i + 1][cp:]
+
             # Second offspring
-            offspring[i+1][:cp] = parents[i+1][:cp]
-            offspring[i+1][cp:] = parents[i][cp:]
-    
+            offspring[i + 1][:cp] = parents[i + 1][:cp]
+            offspring[i + 1][cp:] = parents[i][cp:]
+
     # Case that the number of offspring is odd
     else:
         for i in range(0, len(offspring) - 1, 2):
-            
             # First offspring
             offspring[i][:cp] = parents[i][:cp]
-            offspring[i][cp:] = parents[i+1][cp:]
-            
+            offspring[i][cp:] = parents[i + 1][cp:]
+
             # Second offspring
-            offspring[i+1][:cp] = parents[i+1][:cp]
-            offspring[i+1][cp:] = parents[i][cp:]
-        
+            offspring[i + 1][:cp] = parents[i + 1][:cp]
+            offspring[i + 1][cp:] = parents[i][cp:]
+
         # Final, odd numbered, offspring from the both the worst and best parents
-        offspring[len(offspring)-1][:cp] = parents[len(offspring)-1][:cp]
-        offspring[len(offspring)-1][cp:] = parents[0][cp:]
-    
+        offspring[len(offspring) - 1][:cp] = parents[len(offspring) - 1][:cp]
+        offspring[len(offspring) - 1][cp:] = parents[0][cp:]
+
     # Return mutated offspring
     return induce_mutations(offspring)
-    
+
 
 '''
 "Induce_mutations" adds mutations at a low rate to the offspring of the current generation.
@@ -99,46 +97,48 @@ This works a bit sign flip, changing one or more genes where a mutation is set t
 This is done through generating floats between 1 and 0, and using a rate of mutation as a float.
 When a number is below or at the rate of mutation, the gene at that index is set to mutate.
 '''
+
+
 def induce_mutations(offspring):
-    
     # Declare a rate of mutation, the change at which a mutation will occur as a percentage
     mutation_rate = 0.1
-    
+
     # Make a copy of offspring
     mutated_offspring = np.copy(offspring)
-    
+
     # Declare RNG
     random_gen = np.random.default_rng()
-    
+
     for i in range(len(mutated_offspring)):
-        
+
         # Generate random numbers between 1 and 0, those below mutation rate will be mutated
         mutant_gene_chance = random_gen.random((len(mutated_offspring[0])))
-        
+
         # Indices where mutations will occur
         mutant_indices = np.argwhere(mutant_gene_chance <= mutation_rate)
-        
+
         # Case that mutants will occur
         if len(mutant_indices) != 0:
             for m in mutant_indices:
-                
                 # Flip sign of the gene at that index
-                mutated_offspring[i,m] = 1-mutated_offspring[i,m]
-    
+                mutated_offspring[i, m] = 1 - mutated_offspring[i, m]
+
     return mutated_offspring
 
 
 '''
 Simple helper function to combine arrays
-'''          
+'''
+
+
 def zip_arrays(population, parents, offspring):
     new_population = np.copy(population)
-    
-    half = int(len(new_population)/2)
-    
+
+    half = int(len(new_population) / 2)
+
     new_population[:half] = parents
     new_population[half:] = offspring
-    
+
     return new_population
 
 
@@ -148,43 +148,46 @@ This allows for scaling for very low or veryhigh thresholds
 If the threshold is too low, the population will never generate a specimen that is below threshold.
 This addresses that issue.
 E.g., if threshold is 5% of total costs, ~5% of items will be in the specimens.
-
 Works similar to the induce_mutation function
 '''
+
+
 def initial_population_generation(population_size, produce, costs, threshold):
-    
     # Get the pecentage of items that should be in the specimens
     max_total_cost = int(np.ceil(np.sum(costs)))
-    threshold_percentage = threshold/max_total_cost
-    
+    threshold_percentage = threshold / max_total_cost
+
     # build initial pop as zeros
     initial_population = np.zeros(shape=(population_size, len(produce))).astype(int)
-    
+
     # Declare RNG
     random_gen = np.random.default_rng()
-    
+
     # Iterage over all specimens
     for i in range(len(initial_population)):
-        
         # Generage percentages that the item at this index will be included
         inclusion_chance = random_gen.random((len(initial_population[0])))
-        
+
         # Flip 0 to 1 where the inclusion chance is below the threshold percentage
         initial_population[i, inclusion_chance <= threshold_percentage] = 1
-    
+
     return initial_population
 
 
 '''
 function for rounding total price up to two decimal points
 '''
+
+
 def round_up_2_decimals(a):
-    return np.round(a + 0.5 * 10**(-2), 2)
- 
-   
+    return np.round(a + 0.5 * 10 ** (-2), 2)
+
+
 '''
 main function
 '''
+
+
 def genetic_main(initial_population, costs, values, threshold, population_size, generations):
     current_population = initial_population
     for i in range(generations):
@@ -192,36 +195,36 @@ def genetic_main(initial_population, costs, values, threshold, population_size, 
         current_parents = parent_selection(current_fitness_score, current_population)
         current_offspring = cross_breeding(current_parents)
         current_population = zip_arrays(current_population, current_parents, current_offspring)
-    
+
     final_gen_fitnesses = fitness(current_population, values, costs, threshold)
-    
-    return current_population[np.argmax(final_gen_fitnesses)]    
 
+    return current_population[np.argmax(final_gen_fitnesses)]
 
-# Import csv as dataframe
-items = pd.read_csv("groceries.csv")
+def test(budgetAmount):
+    # Import csv as dataframe
+    items = pd.read_csv("groceries.csv")
 
-# Get numpy arrays from each of the columns
-produce = items['Food'].to_numpy().astype('str')
-costs = items['Price'].to_numpy().astype('float64')
-values = items['weight (lbs)'].to_numpy().astype('float64')
+    # Get numpy arrays from each of the columns
+    produce = items['Food'].to_numpy().astype('str')
+    costs = items['Price'].to_numpy().astype('float64')
+    values = items['weight (lbs)'].to_numpy().astype('float64')
 
-threshold = 35
+    threshold = int(budgetAmount)
 
-population_size = 16
+    population_size = 16
     # Create initial population, each individual specimen is binary encoded
     # Encoding is if item at that index is included in the specimen
-initial_population = initial_population_generation(population_size, produce, costs, threshold)
-generations = 50
+    initial_population = initial_population_generation(population_size, produce, costs, threshold)
+    generations = 50
 
-output = genetic_main(initial_population, costs, values, threshold, population_size, generations)
+    output = genetic_main(initial_population, costs, values, threshold, population_size, generations)
 
-# Print list of produce that has been selected
-print(produce[output == 1])
+    # Print list of produce that has been selected
+    print(produce[output == 1])
 
-# Print total price
-print(round_up_2_decimals(np.sum(costs[output == 1])))
+    # Print total price
+    print(round_up_2_decimals(np.sum(costs[output == 1])))
 
-# Print total value of the list
-print(np.sum(values[output == 1]))
-
+    # Print total value of the list
+    print(np.sum(values[output == 1]))
+    return produce[output == 1], round_up_2_decimals(np.sum(costs[output == 1]));
